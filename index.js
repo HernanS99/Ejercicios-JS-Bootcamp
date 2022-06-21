3879103f6342a82e5f7d86de5265351cf44d285e
HernanS99 <hernan.silva2299@gmail.com>
2022-06-20 22:25:52 -0400

    Primer commit

---
+++ index.js
@@ -0,0 +1,67 @@
+/* 1er ejercicio */
+
+
+/* function pares (){
+    for (let i = 2; i <= 12; i++) {
+        if (i % 2 == 0) {
+          console.log(i);1
+        }
+        
+      }
+}
+pares(); */
+
+
+
+
+/* 2do ejercicio */
+
+
+let cal1 = window.prompt("Ingrese su 1ra califacion del 10 al 100");
+let cal2 = window.prompt("Ingrese su 2da califacion del 10 al 100");
+let cal3 = window.prompt("Ingrese su 3ra califacion del 10 al 100");
+let cal4 = window.prompt("Ingrese su 4ta califacion del 10 al 100");
+let cal5 = window.prompt("Ingrese su 5ta califacion del 10 al 100");
+function calprom(cal1, cal2, cal3, cal4, cal5) {
+  
+  resul = (parseInt(cal1) + parseInt(cal2) + parseInt(cal3) + parseInt(cal4) + parseInt(cal5))/5;
+  let text ="";
+  let result = Number(resul)
+  if(result>=70)
+  {
+      text = "usted aprobo con nota: "
+      console.log(text+""+resul);
+  }else{
+      text = "usted reprobo con nota: "
+      console.log(text+""+resul); 
+  }
+  
+}
+calprom(cal1, cal2, cal3, cal4, cal5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
