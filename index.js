b47c6adab61720e5dd4d24edcf1626138070d3cc
HernanS99 <hernan.silva2299@gmail.com>
2022-06-22 00:01:24 -0400

    Arreglo codigo

--- index.js
+++ index.js
@@ -17,27 +17,27 @@ pares(); */
 /* 2do ejercicio */
 
 
-let cal1 = window.prompt("Ingrese su 1ra califacion del 10 al 100");
-let cal2 = window.prompt("Ingrese su 2da califacion del 10 al 100");
-let cal3 = window.prompt("Ingrese su 3ra califacion del 10 al 100");
-let cal4 = window.prompt("Ingrese su 4ta califacion del 10 al 100");
-let cal5 = window.prompt("Ingrese su 5ta califacion del 10 al 100");
+let cal1 = parseInt(prompt("Ingrese su 1ra califacion del 10 al 100"));
+let cal2 = parseInt(prompt("Ingrese su 2da califacion del 10 al 100"));
+let cal3 = parseInt(prompt("Ingrese su 3ra califacion del 10 al 100"));
+let cal4 = parseInt(prompt("Ingrese su 4ta califacion del 10 al 100"));
+let cal5 = parseInt(prompt("Ingrese su 5ta califacion del 10 al 100"));
+
+calprom(cal1, cal2, cal3, cal4, cal5);
+
 function calprom(cal1, cal2, cal3, cal4, cal5) {
-  
-  resul = (parseInt(cal1) + parseInt(cal2) + parseInt(cal3) + parseInt(cal4) + parseInt(cal5))/5;
+  let resul = (cal1+cal2+cal3+cal4+cal5)/5;
   let text ="";
-  let result = Number(resul)
-  if(result>=70)
+  if(resul>=70)
   {
-      text = "usted aprobo con nota: "
+      text = "usted aprobo con nota: ";
       console.log(text+""+resul);
   }else{
-      text = "usted reprobo con nota: "
-      console.log(text+""+resul); 
+      text = "usted reprobo con nota: ";
+      console.log(text+""+resul);
   }
-  
 }
-calprom(cal1, cal2, cal3, cal4, cal5);
+
 
 
 
