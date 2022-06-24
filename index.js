18d9ff678cf5f46e5def6c02e9aba2d7e7ab4fdb
HernanS99 <hernan.silva2299@gmail.com>
2022-06-24 19:24:35 -0400

    Mejoras

--- index.js
+++ index.js
@@ -26,15 +26,14 @@ let cal5 = parseInt(prompt("Ingrese su 5ta califacion del 10 al 100"));
 calprom(cal1, cal2, cal3, cal4, cal5);
 
 function calprom(cal1, cal2, cal3, cal4, cal5) {
-  let resul = (cal1+cal2+cal3+cal4+cal5)/5;
-  let text ="";
-  if(resul>=70)
-  {
-      text = "usted aprobo con nota: ";
-      console.log(text+""+resul);
-  }else{
-      text = "usted reprobo con nota: ";
-      console.log(text+""+resul);
+  let resul = (cal1 + cal2 + cal3 + cal4 + cal5) / 5;
+  let promRound = Math.round(resul);
+  if (promRound >= 70) {
+    text = "usted aprobo con nota: ";
+    console.log(`${text} ${promRound}`);
+  } else {
+    text = "usted reprobo con nota: ";
+    console.log(`${text} ${promRound}`);
   }
 }
 
