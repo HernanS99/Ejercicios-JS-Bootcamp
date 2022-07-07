323aa8e2a9aa37dd6b82231583d47b8aa31ce73e
HernanS99 <hernan.silva2299@gmail.com>
2022-07-06 22:24:36 -0400

    insert y read basico

--- index.js
+++ index.js
@@ -1,19 +1,98 @@
-let cal1 = parseInt(prompt("Ingrese su 1ra califacion del 10 al 100"));
-let cal2 = parseInt(prompt("Ingrese su 2da califacion del 10 al 100"));
-let cal3 = parseInt(prompt("Ingrese su 3ra califacion del 10 al 100"));
-let cal4 = parseInt(prompt("Ingrese su 4ta califacion del 10 al 100"));
-let cal5 = parseInt(prompt("Ingrese su 5ta califacion del 10 al 100"));
+/* let arrai = ['uno',2,null,0];
 
-calprom(cal1, cal2, cal3, cal4, cal5);
-
-function calprom(cal1, cal2, cal3, cal4, cal5) {
-  let resul = (cal1 + cal2 + cal3 + cal4 + cal5) / 5;
-  let promRound = Math.round(resul);
-  if (promRound >= 70) {
-    text = "usted aprobo con nota: ";
-    console.log(`${text} ${promRound}`);
-  } else {
-    text = "usted reprobo con nota: ";
-    console.log(`${text} ${promRound}`);
+function print(arrai)
+{
+  for(i=0; i < arrai.length; i++)
+  {
+    console.log(arrai[i])
   }
+}
+
+print(arrai); */
+
+
+/* let arrai = [90,89,80,23,20]
+
+function aprob(arrai)
+{
+  let result = arrai.filter(arrai => arrai>=70);
+  console.log(result);
+}
+aprob(arrai);
+ */
+
+
+/* 
+let contar = (["mexicano", "argentino", "venezolano", "peruano", "mexicano", "argentino", "peruano", "venezolano","mexicano", "peruano", "argentino"]);
+
+function counter(contar) {
+  let contM = 0;
+  let contA = 0;
+  let contV = 0;
+  let contP = 0;
+  for (i = 0; i < contar.length; i++) {
+    switch (contar[i]) {
+      case ("mexicano"):
+        contM = contM + 1;
+        break
+      case ("argentino"):
+        contA = contA + 1;
+        break
+      case ("venezolano"):
+        contV = contV + 1;
+        break
+      case ("peruano"):
+        contP = contP + 1;
+        break
+    }
+  }
+  console.log("Cantidad argentinos: ",contA," Cantidad Mexicanos: " ,contM, "Cantidad venezolanos : ",contV, "Cantidad peruanos ",contP)
+}
+
+counter(contar)
+
+ */
+
+
+
+
+
+
+
+/*  let name = window.prompt("Ingrese su nombre");
+let edad = parseInt(prompt("Ingrese su edad"));
+let ocupacion = window.prompt("Ingrese su ocupacion");
+
+
+function show(nameU,edadU,ocupacionU)
+{
+  person = {
+    nombre: "",
+    edad: 0,
+    ocupacion: ""
+  }
+  person.nombre=nameU;
+  person.edad=edadU;
+  person.ocupacion=ocupacionU;
+  console.log(person);
+}
+
+show(name,edad,ocupacion)
+
+ */
+
+
+
+let infolibros = [];
+
+const addbook = (titulo, autor) => {
+  let libros = {
+    titulo: titulo,
+    autor: autor
+  };
+  infolibros.push(libros)
+}
+
+const imprBook = () => {
+  console.log(infolibros)
 }
